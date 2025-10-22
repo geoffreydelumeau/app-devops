4a2462991635525809d9c1626ec1ec94d00a1a5c
Geoffrey DELUMEAU <gdelumeau@campus-eni.fr>
2025-10-22 17:20:50 +0200

    first commit

---
+++ src/main/java/fr/eni/devops/appdevops/controller/TestController.java
@@ -0,0 +1,27 @@
+package fr.eni.devops.appdevops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public Object hello(){
+        Map<String, String> object = new HashMap<>();
+
+        object.put("name", "Formateur");
+        object.put("email", "formateur@campus-eni.fr");
+        object.put("Date", String.valueOf(LocalDate.now()));
+        object.put("Campus", "ENI-ECOLE");
+
+        return object;
+
+    }
+
+
+}
